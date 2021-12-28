f115fd8b15d054ccd303c522861af44ef0c0c27b
GjergjiSh <gjergji.shkurti@steinbeis-ias.de>
2021-12-28 16:47:23 +0100

    added smoothing

--- Camera.py
+++ Camera.py
@@ -48,10 +48,18 @@ if __name__ == '__main__':
 
     screen_w, screen_h = autopy.screen.size()
     print(screen_h, screen_w)
+
+    #if your screen is 1280x720p you can't move the mouse to the point (1280, 720),
+    # the "maximum" point that it accepts
+    # it's actually (screenWidth - 1, screenHeight - 1) or (1279, 719)p in this case.
     screen_w -= 1
     screen_h -= 1
-    downsize_var = 100 #pixels
 
+    prev_locx, prev_locy = 0,0
+    curr_locx, curr_locy = 0,0
+
+    downsize_var = 100 #pixels
+    smooth_var = 7
 
     while True:
         frame = cam.capture_frame()
@@ -66,10 +74,22 @@ if __name__ == '__main__':
         (cx, cy, click) = detector.click_detected(frame)
 
         if ((cx is not None) and (cy is not None)):
+
+            # Map downsized rectangle pixel range to screen range
             icx = np.interp(cx, (downsize_var, cam.cap_w-downsize_var), (0,screen_w))
             icy = np.interp(cy, (downsize_var, cam.cap_h-downsize_var), (0,screen_h))
 
-            autopy.mouse.move(icx,icy)
+            # Smoothen mapped values to avoid shaky mouse
+            curr_locx = prev_locx + (icx - prev_locx) / smooth_var
+            curr_locy = prev_locy + (icy - prev_locy) / smooth_var
+
+            autopy.mouse.move(curr_locx,curr_locy)
+
+            if click:
+                autopy.mouse.click()
+
+            # Track smoothed values
+            prev_locx, prev_locy = curr_locx,curr_locy
 
         #print(cx, cy, click)
         cam.display_frame(frame)
